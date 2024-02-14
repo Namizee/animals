2795428ec973b16ceda79600defb6e9ba8678cab
Namizee <sania.kogotko123@mail.ru>
2024-02-14 16:56:41 +0300

    add functions show and delete animal

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
 use App\Entity\User;
 use App\Repository\AnimalRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/profile')]
 class ProfileController extends AbstractController
@@ -20,5 +24,29 @@ class ProfileController extends AbstractController
         $userAnimals = $animalRepository->findBy(['user' => $user], ['publicationDate' => 'DESC']);
 
         return $this->render('profile/profile.html.twig', ['animals' => $userAnimals]);
+    }
+
+    #[Route('/animal/{id<\d+>}', name: 'profile_animal_show', methods: ['GET'])]
+    #[isGranted('show', 'animal')]
+    public function show(Animal $animal): Response
+    {
+        return $this->render('profile/animal/show.html.twig', ['animal' => $animal]);
+    }
+
+    #[Route('/animal/{id<\d+>}/delete', name: 'profile_animal_delete', methods: ['POST'])]
+    #[isGranted('delete', 'animal')]
+    public function delete(Request $request, Animal $animal, EntityManagerInterface $entityManager): Response
+    {
+        $token = $request->request->get('token');
+
+        if (!$this->isCsrfTokenValid('delete', $token)) {
+            return $this->redirectToRoute('profile_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        $entityManager->remove($animal);
+        $entityManager->flush();
+        $this->addFlash('success', 'Ваша запись успешно удалена!');
+
+        return $this->redirectToRoute('profile_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Security/AnimalVoter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Animal;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class AnimalVoter extends Voter
+{
+    public const SHOW = 'show';
+    public const EDIT = 'edit';
+    public const DELETE = 'delete';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return $subject instanceof Animal && in_array($attribute, [self::SHOW, self::EDIT, self::DELETE]);
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        return $subject->getUser() === $user;
+    }
+}
